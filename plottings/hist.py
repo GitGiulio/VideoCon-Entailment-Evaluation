3adac112355442e15bb386ab1256c7c030dbbb25
GitGiulio <69458420+GitGiulio@users.noreply.github.com>
2024-06-06 08:59:31 +0200

    hist automated

--- plottings/hist.py
+++ plottings/hist.py
@@ -5,27 +5,38 @@ plt.style.use('dark_background')
 
 df = pd.read_csv('../data/complete_df.csv')
 
-
-ff_sets = ['ff_entail_diff_llava', 'ff_entail_diff_clip_flant', 'ff_entail_diff_instructblip_flant',
-           'ff_cap_llava', 'ff_neg_cap_llava', 'ff_cap_clip_flant', 'ff_neg_cap_clip_flant',
-           'ff_cap_instructblip_flant', 'ff_neg_cap_instructblip_flant']
-video_sets = ['videocon_synth', 'videocon_real',
-              'clip_flant synth', 'clip_flant real',
-              'instructblip_flant synth','instructblip_flant real',
-              'llava synth', 'llava real']
-
-come_fare = 'videocon(Vc_1,S)'
-
-data1 = 'instructblip_flant(Vc_r1,R)'
-data2 = 'clip_flant(Vc_1,R)'
-
 # TODO fare cartelle per modelli e automatizzare anche questo
 
 # Unconditioned::=  REAL = #75fa85    |  SYNTH = #f58b45   |  DIFF = pink
 # Conditioned::=  REAL = #2af542    |  SYNTH = #e84c31   |  DIFF = magenta
 
+# ciclo per creare tutti gli istogrammi per i video
+for model in ['llava','clip_flant','instructblip','mean','videocon','mean_wv']:
+    for video_type in ['c','u']:
+        for caption in ['R','S']:
+            color = '#2ad0f5'
+            title = ''
 
-plt.hist(df[data1],bins=100,color='#2ad0f5')
-plt.title('instructblip_flant(Vc_r1,R)')
+            if model in ['mean','mean_wv']:
+                plt.hist(df[f'{model}(V{video_type},{caption})'],bins=200,color=color)
+                title = f'{model}(V{video_type},{caption})'
+            else:
+                plt.hist(df[f'{model}(V{video_type}_1,R)'],bins=200,color=color)
+                title = f'{model}(V{video_type}_1,R)'
 
-plt.savefig('plots/histograms/instructblip_flant(Vc_r1,R).png')
+            plt.title(title)
+
+            plt.savefig(f'plots/histograms/{model}/Video/{title}.png')
+            plt.clf()
+
+# ciclo per creare tutti gli istogrammi per i frame
+for model in ['llava','clip_flant','instructblip','mean_wv']:
+    for caption in ['R','S']:
+        color = '#2ad0f5'
+        title = ''
+
+        plt.hist(df[f'{model}(F,{caption})'], bins=200, color=color)
+        title = f'{model}(F,{caption})'
+
+        plt.savefig(f'plots/histograms/{model}/Frame/{title}.png')
+        plt.clf()
